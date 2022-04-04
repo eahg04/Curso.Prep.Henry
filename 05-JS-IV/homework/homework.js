48a7bd17dc3b8832b5c549b78f2b3a74c46730db
eahg04 <arleyherrera@gmail.com>
2022-04-04 17:56:59 -0500

    todos resueltos

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -88,7 +88,7 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
-  if (usuario["pasword"] === password) {
+  if (usuario["password"] === password) {
     return true;
   } else {
     return false;
